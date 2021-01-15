c53efec9260b7534e70fa957f619b7c61a291478
raysarinas <raysarinas97@gmail.com>
2021-01-14 20:46:22 -0700

    lab 1 step 10+11

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,6 +2,8 @@ import requests
 
 #print(requests.__version__)
 
-r = requests.get('https://www.google.com')
-print(r)
+#r = requests.get('https://www.google.com')
+#print(r)
 
+r = requests.get('https://raw.githubusercontent.com/raysarinas/cmput404/master/lab1/lab1.py')
+print(r.text)
